d719e44f93a196ba73d627d6b0f66b14b8d2eae7
Aaron_God <97518549+HtetAung-Aaron@users.noreply.github.com>
2022-09-19 16:35:46 +0630

    Create protofolio.js

---
+++ protofolio.js
@@ -0,0 +1,18 @@
+/*  
+for long story short i use express and node js 
+
+*/
+
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Application started and Listening on port 3000");
+});
+
+// serve your css as static
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/app.html");
+});
